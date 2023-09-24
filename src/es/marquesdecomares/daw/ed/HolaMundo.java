4ed83642d70ab1e4bf3de5f93137f91e4654166c
virtualManolo <manolo@manolo-VirtualBox>
2023-09-24 13:48:44 +0200

    Modificado desde maquina virtual

--- src/es/marquesdecomares/daw/ed/HolaMundo.java
+++ src/es/marquesdecomares/daw/ed/HolaMundo.java
@@ -4,7 +4,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
-		System.out.println("Adiós mundo");
+		System.out.println("Adiós mundo desde el espacio virtual");
 	}
 
 }
